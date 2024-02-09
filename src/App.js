46022970a984b7a2d577092a2355af42c50a9f16
KLaNyange <nyangealixelambrette@gmail.com>
2024-02-09 14:44:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
 import './App.css';
 
 function App() {
+  const [count, countDispatch] = useReducer(countReducer, 0) 
+  // {count} est le state qui va commencer à 0
+  // le reducer(fonction) sert à modifier en fonction d'une action
+  // dispatch (function)va permettre d'envoyer une action au reducer
+
+
+  console.log(count);
+
+  function countReducer(state, action){ // Attention l'ordre des paramètres IMPORTANT d'abord state puis action
+    switch (action.type) {
+      case "augmenter":
+        console.log(action);
+        return state + action.payload
+      case "diminuer":
+        console.log(action);
+        return state - action.payload
+      default:
+        break;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mon compteur{count}</h1>
+      <h1>Mon compteur{count}</h1>
+      <button onClick={()=>countDispatch({type:'augmenter', payload:5})}>Augmenter</button> {/* payload -> données supplémentaire */}
+      <button onClick={()=>countDispatch({type:'diminuer', payload: 100})}>Diminuer</button>
     </div>
   );
 }
